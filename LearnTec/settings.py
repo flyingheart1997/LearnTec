788e7973a919947f08b540d0d0e7abe7a61f1219
KOUSHIK MONDAL <98386440+flyingheart1997@users.noreply.github.com>
2022-02-04 10:53:20 +0530

    Update settings.py

--- LearnTec/settings.py
+++ LearnTec/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-psq*f966k^s71^)-j#w##&_gk*(i-^eum(!d_(l+ao$od0y&9_'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
